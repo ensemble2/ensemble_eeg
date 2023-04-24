360109bfc4ed2533a8996f4ee9a963d5e06e1a1a
Bauke van der Velde <b.vandervelde@uu.nl>
2023-04-24 22:57:33 +0200

    first full verion

--- brm_to_edf.py
+++ brm_to_edf.py
@@ -4,14 +4,23 @@ from collections import Counter
 import glob
 import os
 from collections import namedtuple
+import shutil
+import numpy as np
+from datetime import datetime, timezone
+import ensemble_edf
+
+fd = "/Users/bauke/_Research/aEEG_check/115-044-249-1_1_1_RAW.brm"
 
 
 def convert_brm_to_edf(fd):
+    filename = os.path.splitext(os.path.basename(fd))[0]
     file_exists = os.path.isfile(fd)
 
     if file_exists:
         file_dir = os.path.dirname(fd)
         tmp_dir = os.path.join(file_dir, 'tmp')
+        if os.path.isdir(tmp_dir):
+            shutil.rmtree(tmp_dir)
 
         with zipfile.ZipFile(fd, 'r') as zip_ref:
             zip_ref.extractall(tmp_dir)
@@ -22,7 +31,9 @@ def convert_brm_to_edf(fd):
         device = parse_xml(device_xml)
 
         dat_files = []
-        is_fs_64hz = input('is the sampling frequency 64 Hz? [y/N]') == 'y'
+        is_fs_64hz =\
+            input('is the sampling frequency 64 Hz? [y/N]: ').lower() == 'y'
+
         if is_fs_64hz:
             dat_files.append(glob.glob(
                 os.path.join(tmp_dir, 'DATA_RAW_EEG_LEFT*.dat')))
@@ -39,8 +50,25 @@ def convert_brm_to_edf(fd):
 
         assert n_data_files_left == n_data_files_right
 
-        dat_files = dat_files_left + dat_files_right
+        for i in range(0, n_data_files_left):
+            both_dat_files = [dat_files_left[i], dat_files_right[i]]
+            data = extract_brm_file(index, device, both_dat_files)
 
+            if i == 0:
+                output_filename = os.path.splitext(fd)[0] + '.edf'
+            else:
+                output_filename = os.path.splitext(fd)[0] + '_' + str(i) +\
+                    '.edf'
+
+            hdr = prepare_edf_header(data)
+            signal_header = prepare_edf_signal_header(data, device)
+            header = ensemble_edf.Header(*hdr, signal_header)
+
+            # write header to file
+            ensemble_edf.write_edf_header(output_filename, header)
+
+            # write data to file
+            write_brm_data_to_edf(output_filename, data)
         # TODO: finish up this piece of code
     else:
         raise ValueError('file not found')
@@ -63,6 +91,7 @@ def parse_xml(xml):
         for name, vals in tag_dict:
             if vals == 1:
                 parsed_xml.append(root[counter].text)
+                counter = counter + 1
             else:
                 parsed_child = []
                 child_elems = root.findall(name)
@@ -71,6 +100,7 @@ def parse_xml(xml):
                     child_elem_vals = [el.text for el in child_elem]
                     tup = namedtuple(child_elem.tag, child_elem_tags)
                     parsed_child.append(tup(*child_elem_vals))
+                    counter = counter + 1
                 parsed_xml.append(parsed_child)
 
     else:
@@ -79,25 +109,120 @@ def parse_xml(xml):
     return Parsed_XML(*parsed_xml)
 
 
-def extract_brm_file(index, device, tmp_dir_path, dat_files):
+def extract_brm_file(index, device, dat_files):
     filenames = [fd.FileName for fd in index.FileDescription]
 
     counter = 0
+    data = [[] for _ in dat_files]
+
     for dat_file in dat_files:
         df = os.path.basename(dat_file)
+        file = index.FileDescription[filenames.index(df)]
+        file = file._replace(FileName=dat_file)
         print(f'Extracting {df} datastream')
-        data[counter] = \
-            get_brm_data(index.FileDescription[filenames.index(df)],
-                         tmp_dir_path, device)
+        data[counter] = get_brm_data(file, device)
+        counter = counter + 1
 
-    # TODO: finish up this piece of code
+    return data
 
 
-def get_brm_data(file, tmp_dir_path, device):
+def get_brm_data(file, device):
     DAUSampleHz = 512
 
     Data = namedtuple('data', list(file._fields) + ['sampleHz', 'data'])
     data = list(file)
     data.append(int(DAUSampleHz / int(file.SamplePeriod512thSeconds)))
+    data.append(get_numerical_data(file, device))
 
-    # TODO: finish up this piece of code
+    return Data(*data)
+
+
+def get_numerical_data(file, device):
+    f = open(file.FileName, 'rb')
+
+    match file.FileType:
+        case 'FloatMappedToInt16' | 'Int16':
+            data = np.fromfile(f, dtype=np.int16)
+        case 'Float32':
+            data = np.fromfile(f, dtype=np.float32)
+        case _:
+            raise Exception(f'Unrecognized file format {file.FileType}')
+
+    return data
+
+
+def prepare_edf_header(data):
+    n_channels = 2
+    version = '0'
+    PID = 'X X X X'
+    RID = 'Startdate X X X X'
+    start_date = datetime.strftime(
+        datetime.fromtimestamp(int(data[0].StartTime),
+                               timezone.utc), format='%d.%m.%y')
+    start_time = datetime.strftime(
+        datetime.fromtimestamp(int(data[0].StartTime),
+                               timezone.utc), format='%H.%M.%S')
+    bytes_in_header = ensemble_edf.HEADER_SIZE + n_channels *\
+        ensemble_edf.SIGNAL_HEADER_SIZE
+    reserved = None
+    fs = data[0].sampleHz
+    n_records = int(np.floor(len(data[0].data)/fs))
+    dur_data_record = '1'
+    n_signals = len(data)
+    header = [version, PID, RID, start_date, start_time, bytes_in_header,
+              reserved, n_records, dur_data_record, n_signals]
+
+    return header
+
+
+def prepare_edf_signal_header(data, device):
+    channel_ids = [channel.ID for channel in device.Channel]
+    units = data[0].Units
+
+    # replace non-ascii characters
+    if 'µ' in units:
+        units = units.replace('µ', 'u')
+
+    signal_headers = [[] for _ in data]
+    for i in range(0, len(data)):
+        ichan = channel_ids.index(data[i].ChannelTitle)
+        if data[i].ChannelTitle == 'Left':
+            label = 'F3'
+        else:
+            label = 'F4'
+
+        transducer_type = None
+        physical_dimension = units
+        physical_min = round(-5e6 / float(device.Channel[ichan].Gain) / 2)
+        physical_max = round(5e6 / float(device.Channel[ichan].Gain) / 2)
+        digital_min = -(2**15-1)
+        digital_max = (2**15-1)
+        prefiltering = None
+        nr_samples = data[i].sampleHz
+        reserved = None
+        sig_header = [label, transducer_type, physical_dimension,
+                      physical_min, physical_max, digital_min,
+                      digital_max, prefiltering, nr_samples,
+                      reserved]
+        signal_headers[i] = ensemble_edf.SignalHeader(*sig_header)
+
+    return tuple(signal_headers)
+
+
+def write_brm_data_to_edf(filename, data):
+    file_exists = os.path.isfile(filename)
+    dat1 = data[0].data
+    dat2 = data[1].data
+
+    if file_exists:
+        fs = data[0].sampleHz
+        n_records = int(np.floor(len(data[0].data)/fs))
+        fd = open(filename, 'ab')
+
+        index = 0
+        for i_record in range(0, n_records):
+            fd.write(dat1[index:(index+fs)])
+            fd.write(dat2[index:(index+fs)])
+            index = (i_record + 1) * fs
+
+        fd.close()
