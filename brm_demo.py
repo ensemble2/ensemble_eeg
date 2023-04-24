e847f1fa9b2c219a7f837750bbf0a628b9998e8f
Bauke van der Velde <b.vandervelde@uu.nl>
2023-04-24 22:58:03 +0200

    just some demo scripts

---
+++ brm_demo.py
@@ -0,0 +1,8 @@
+import brm_to_edf
+from importlib import reload
+import glob
+reload(brm_to_edf)
+
+fd = '/Users/bauke/_Research/EEG_aEEG/0002_2.brm'
+
+brm_to_edf.convert_brm_to_edf(fd)
--- demo.py
+++ demo.py
@@ -9,3 +9,8 @@ files = glob.glob(
 for file in files:
     ensemble_edf.fix_edf_header(file)
     ensemble_edf.anonymize_edf_header(file)
+
+fd = "/Users/bauke/Downloads/data/subj_101E100011/subj_101E100011_ses-diag_acq-aEEG_run-1_eeg.edf"
+
+ensemble_edf.rename_for_ensemble(fd)
+ensemble_edf.anonymize_edf_header(fd)
