4adeea48c56c8681d96e7be882e65a21f91b48eb
bavadeve <bauke.vandervelde@gmail.com>
2023-08-31 11:24:58 +0200

    new demo.py and improved .gitignore

--- demo.py
+++ demo.py
@@ -4,13 +4,9 @@ import glob
 reload(ensemble_edf)
 
 files = glob.glob(
-    '/Users/bauke/Downloads/Bauke ENSEMBLE/subj_101E000031/*.edf')
+    '/path/to/edffiles/*.edf')
 
 for file in files:
     ensemble_edf.fix_edf_header(file)
     ensemble_edf.anonymize_edf_header(file)
-
-fd = "/Users/bauke/Downloads/data/subj_101E100011/subj_101E100011_ses-diag_acq-aEEG_run-1_eeg.edf"
-
-ensemble_edf.rename_for_ensemble(fd)
-ensemble_edf.anonymize_edf_header(fd)
+    ensemble_edf.rename_for_ensemble(file)
