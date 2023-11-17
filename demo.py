eb3b40d2f1d31bbe6441a3401c999b28e2b60f47
bavadeve <bauke.vandervelde@gmail.com>
2023-11-17 09:27:26 +0100

    changed demo to be more understandable

--- demo.py
+++ demo.py
@@ -2,9 +2,12 @@ import glob
 import ensemble_edf
 import os
 
-files = glob.glob(os.path.join("EDFfolder", "*.edf"))
+# !!! change this to the location of the edf directory you want to change
+path2edffiles = "path/2/edf/files"
+
+files = glob.glob(os.path.join(path2edffiles, "*.edf"))
 
 for file in files:
     ensemble_edf.fix_edf_header(file)
     ensemble_edf.anonymize_edf_header(file)
-    ensemble_edf.rename_for_ensemble(file)
+    # ensemble_edf.rename_for_ensemble(file)
