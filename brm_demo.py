47200c07eb8b5c22369e11b041ed43c3547766f5
bavadeve <bauke.vandervelde@gmail.com>
2024-02-06 14:35:45 +0100

    remove unncessary dependencies

--- brm_demo.py
+++ brm_demo.py
@@ -1,14 +1,12 @@
 import glob
 from importlib import reload
 import brm_to_edf
-import os
 
 reload(brm_to_edf)
 
 files = glob.glob(
-    "/Users/bvelde3/SCORED/*.brm"
+    "/Users/bvelde3/_Research/EEG_Ensemble_WKZ/Data/*.brm"
 )  # Change this to where your BRM files are
-files = files[0:1]
 
 for file in files:
-    brm_to_edf.convert_brm_to_edf(file, is_fs_64hz=False)
+    brm_to_edf.convert_brm_to_edf(file, is_fs_64hz=True)
