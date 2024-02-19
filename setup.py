8dcab6517a8d307fc33b8d42da4bfc82c24f009c
bavadeve <bauke.vandervelde@gmail.com>
2024-02-19 13:49:26 +0100

    Change version after bugfixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     py_modules=["ensemble_edf", "brm_to_edf"],
     python_requires=">3.1",
     install_requires=["defusedxml", "numpy"],
-    version="0.1.2",
+    version="0.1.3",
     author="Bauke van der Velde",
     author_email="b.vandervelde-3@umcutrecht.nl",
 )
