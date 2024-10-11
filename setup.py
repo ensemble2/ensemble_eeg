47c1b382bb2ac24309dea31a7f5101c555a04fbc
bavadeve <bauke.vandervelde@gmail.com>
2024-10-11 12:32:23 +0200

    new package version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     py_modules=["ensemble_edf", "brm_to_edf"],
     python_requires=">3.1",
     install_requires=["defusedxml", "numpy"],
-    version="0.1.5",
+    version="0.1.6",
     author="Bauke van der Velde",
     author_email="b.vandervelde-3@umcutrecht.nl",
     packages=["ensemble_eeg"],
